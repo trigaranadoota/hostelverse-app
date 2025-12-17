5d52f230c8edb902faeefd43c09c04ee983232d6
subba rao <subbarao.78925@gmail.com>
2025-12-17 21:27:24 +0000

    Try fixing this error: `Runtime ReferenceError: Label is not defined. Er

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -23,6 +23,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import {
   Select,
   SelectContent,
