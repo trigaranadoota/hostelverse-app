a99c22ffa76db01406f994b7721acf697db81239
subba rao <subbarao.78925@gmail.com>
2025-12-17 19:57:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2322173444-3d6ba",
+  "appId": "1:722655015060:web:ea97a96b90f804ddd7c481",
+  "apiKey": "AIzaSyBFsWkjDCnqOdwCA182YW8HIw8V-s-_D20",
+  "authDomain": "studio-2322173444-3d6ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "722655015060"
+};
