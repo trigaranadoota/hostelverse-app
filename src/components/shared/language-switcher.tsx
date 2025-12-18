7bbd4bfb8bee597ae1ecf265ca80754c7411df62
subba rao <subbarao.78925@gmail.com>
2025-12-18 15:45:57 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/shared/language-switcher.tsx
+++ src/components/shared/language-switcher.tsx
@@ -8,8 +8,20 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Globe } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Skeleton } from "../ui/skeleton";
 
 export function LanguageSwitcher() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <Skeleton className="w-24 h-9" />;
+  }
+
   return (
     <Select defaultValue="en">
       <SelectTrigger className="w-auto h-9 gap-2">
