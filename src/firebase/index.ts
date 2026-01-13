7b8b602d9dde8e447f3fca91ea38a6bcf7327c26
subba rao <subbarao.78925@gmail.com>
2026-01-13 20:12:38 +0000

    Still same error

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,17 +1,27 @@
 'use client';
 
-import { getFirebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+
+// Hardcoded Firebase configuration to ensure availability in all environments.
+const firebaseConfig = {
+  apiKey: "AIzaSyBFsWkjDCnq0dwCA182YW8HIw8V-s-_D20",
+  authDomain: "studio-2322173444-3d6ba.firebaseapp.com",
+  projectId: "studio-2322173444-3d6ba",
+  storageBucket: "studio-2322173444-3d6ba.appspot.com",
+  messagingSenderId: "1062085732383",
+  appId: "1:1062085732383:web:014c2d46a815a513c1c4f5",
+  measurementId: "G-9T4V5G43P9"
+};
+
 
 export function initializeFirebase() {
   if (getApps().length) {
     return getSdks(getApp());
   }
   
-  const firebaseConfig = getFirebaseConfig();
   if (!firebaseConfig.apiKey) {
     throw new Error('Firebase API key is not available in the environment.');
   }
