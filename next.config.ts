dcd149fded1a91cc65124ea5e6343f058d125cd1
subba rao <subbarao.78925@gmail.com>
2026-01-13 20:08:31 +0000

    it is showing this when I am trying to sign up, the same showed when I t

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,17 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    firebaseConfig: {
+      apiKey: "AIzaSyBFsWkjDCnq0dwCA182YW8HIw8V-s-_D20",
+      authDomain: "studio-2322173444-3d6ba.firebaseapp.com",
+      projectId: "studio-2322173444-3d6ba",
+      storageBucket: "studio-2322173444-3d6ba.appspot.com",
+      messagingSenderId: "1062085732383",
+      appId: "1:1062085732383:web:014c2d46a815a513c1c4f5",
+      measurementId: "G-9T4V5G43P9"
+    }
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
